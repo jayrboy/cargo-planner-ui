f58a8236cd84c5233c96ba4c4d3312e522186060
jayrboy <j.jayox_@hotmail.com>
2024-12-04 03:58:43 +0700

    add ResultList

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Header from './views/common/header/Header';
 import NotFound from './views/not-found/NotFound';
 import CargoList from './views/cargo-list/CargoList';
 import ItemEdit from './views/cargo-edit/CargoEdit';
+import ResultList from './views/result-list/ResultList';
 
 function App() {
   return (
@@ -21,7 +22,7 @@ function App() {
         <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="/cargo-list" element={<CargoList />} />
         <Route path="/cargo-edit/:id" element={<ItemEdit />} />
-        <Route path="/result-list" element={<>Result List</>} />
+        <Route path="/result-list" element={<ResultList />} />
         <Route
           path="/result/:instanceId/:resultId/:truckIndex"
           element={<>View Result</>}
---
+++ src/views/result-list/ResultList.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Result } from '../../models/ListItem';
+import { getResults } from '../../services/ResultService';
+import { Button, ListGroup, ButtonGroup } from 'react-bootstrap';
+import { getUserId } from '../../services/UserService';
+
+function ResultList() {
+  const userId: string = getUserId();
+  const navigate = useNavigate();
+  const [results, setResults] = useState<Result[]>([]);
+
+  useEffect(() => {
+    const fetchInstances = async () => {
+      const loadedResults: Result[] = await getResults(userId);
+      setResults(loadedResults);
+    };
+
+    fetchInstances();
+  }, []);
+
+  function handleView(instanceId: string, resultId: string) {
+    navigate('/result/' + instanceId + '/' + resultId + '/0');
+  }
+
+  return (
+    <ListGroup>
+      {results.map((result, i) => {
+        return (
+          <ListGroup.Item key={i}>
+            {result.display + ', Instance [' + result.instanceId + ']'}
+
+            <ButtonGroup className="float-right">
+              <Button
+                variant="primary"
+                onClick={() => handleView(result.instanceId, result.id)}
+              >
+                View result
+              </Button>
+            </ButtonGroup>
+          </ListGroup.Item>
+        );
+      })}
+    </ListGroup>
+  );
+}
+
+export default ResultList;
